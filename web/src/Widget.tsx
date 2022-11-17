c7abc6c8ab123058561a5bd0a52fef42b7396a15
gabrieladutra <gabriela_ddutra@outlook.com>
2022-11-17 19:38:35 -0300

    Added main button

--- web/src/Widget.tsx
+++ web/src/Widget.tsx
@@ -2,11 +2,12 @@ import {ChatTeardropDots} from 'phosphor-react'
 
 export function Widget(){
   return (
-    <div className='absolute bottom-5 right-5'>
-<button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center'>
-  <ChatTeardropDots className='w-6 h-6'/>
-  <span className='max-w-0 overflow-hidden'>
-	<span className='pl-2'></span>
+  <div className="absolute bottom-5 right-5">
+	<button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+  <ChatTeardropDots className="w-6 h-6"/>
+
+  <span className="max-w-0 overflow-hidden group-hover:max-w-xs trasnsition-all duration-500 ease-linear">
+	<span className="pl-2"></span>
 		Feedback</span>
 	</button>
     </div>
