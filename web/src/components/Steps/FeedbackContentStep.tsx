cb481b994ee9233c0af2fda020817c7e3c57ad7c
gabrieladutra <gabriela_ddutra@outlook.com>
2022-11-22 20:33:09 -0300

    Added feedback restart button

--- web/src/components/Steps/FeedbackContentStep.tsx
+++ web/src/components/Steps/FeedbackContentStep.tsx
@@ -1,11 +1,14 @@
+import { ArrowLeft } from "phosphor-react";
 import { CloseButton } from "../CloseButton";
 import { FeedbackType, feedbackTypes } from "../WidgetForm";
 
 interface FeedbackContentStepProps {
-    feedbackType: FeedbackType;
+    feedbackType: FeedbackType
+    onFeedbackRestartRequested: () => void
 }
 export function FeedbackContentStep({
-    feedbackType,
+    feedbackType, 
+    onFeedbackRestartRequested
 }: FeedbackContentStepProps) {
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
@@ -13,6 +16,12 @@ export function FeedbackContentStep({
         //Fragment
         <>
             <header>
+              <button type="button" 
+              className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100" 
+              onClick={onFeedbackRestartRequested}>
+              
+                <ArrowLeft weight="bold" className="w-4 h4"/>
+              </button>
                 <span className="text-xl leading-6 flex items-center gap-2">
                   <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6"></img>
                   {feedbackTypeInfo.title}</span>
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -38,6 +38,9 @@ export type FeedbackType = keyof typeof feedbackTypes
 export function WidgetForm(){
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
   
+  function handleRestartFeedback(){
+    setFeedbackType(null)
+  }
   return (
   <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
   
@@ -45,7 +48,9 @@ export function WidgetForm(){
 {!feedbackType ?(
   <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
   ) : (
-  <FeedbackContentStep feedbackType={feedbackType} />)
+  <FeedbackContentStep feedbackType={feedbackType} 
+  onFeedbackRestartRequested={handleRestartFeedback}
+  />)
   }
 
 <footer className="text-xs text-neutral-400">
