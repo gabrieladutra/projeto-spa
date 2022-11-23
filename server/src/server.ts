3c557023ffbd47d788dc1d4d5ef22f8df4a52e76
gabrieladutra <gabriela_ddutra@outlook.com>
2022-11-23 19:49:32 -0300

    Added send email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer'
 import express from 'express'
 
 const app = express();
 
 app.use(express.json())
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "e692c0bcadd1bc",
+    pass: "9c72523c22b6ba"
+  }
+});
+
 app.post('/feedbacks', async (req,res) => {
   const {type, comment, screenshot} = req.body
   const feedback =  await prisma.feedback.create({
@@ -12,6 +22,17 @@ app.post('/feedbacks', async (req,res) => {
       comment,
       screenshot,
       }
+    })
+   await transport.sendMail({
+      from:'Equipe FeedGet',
+      to: 'Gabriela Dutra <gabriela_ddutra@outlook.com>',
+      subject: 'Novo feedback',
+      html:[
+        `<div style="font-family:sans-serif; font-size: 16px; color:#111;">`,
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+        `</div>`
+      ].join('\n')
     })
     return res.status(201).json({data: feedback})
   })
