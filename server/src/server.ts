31ddc2a166a5b1cc9bff852f97b84fe86292f039
gabrieladutra <gabriela_ddutra@outlook.com>
2022-11-23 19:26:45 -0300

    Added backend initial config

---
+++ server/src/server.ts
@@ -0,0 +1,20 @@
+import { prisma } from './prisma';
+import express from 'express'
+
+const app = express();
+
+app.use(express.json())
+app.post('/feedbacks', async (req,res) => {
+  const {type, comment, screenshot} = req.body
+  const feedback =  await prisma.feedback.create({
+      data:{
+      type,
+      comment,
+      screenshot,
+      }
+    })
+    return res.status(201).json({data: feedback})
+  })
+app.listen(3333, () => {
+  console.log('HTTP server running!');
+})
