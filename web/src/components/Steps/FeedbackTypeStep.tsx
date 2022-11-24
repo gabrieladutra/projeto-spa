000eae344c956b17ef4ee57030dfc82744d260b9
gabrieladutra <gabriela_ddutra@outlook.com>
2022-11-24 19:16:20 -0300

    Fixing loading button

--- web/src/components/Steps/FeedbackTypeStep.tsx
+++ web/src/components/Steps/FeedbackTypeStep.tsx
@@ -1,36 +1,33 @@
-import { CloseButton } from "../CloseButton"
-import { FeedbackType, feedbackTypes } from "../WidgetForm"
-
+import { CloseButton } from "../CloseButton";
+import { FeedbackType, feedbackTypes } from "../WidgetForm";
 interface FeedbackTypeStepProps {
-  onFeedbackTypeChanged: (type: FeedbackType) => void
+  onFeedbackTypeChanged: (type: FeedbackType) => void;
 }
-export function FeedbackTypeStep({onFeedbackTypeChanged}: FeedbackTypeStepProps){
+
+export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProps) {
   return (
-    //Fragment
     <>
-    <header>
-    <span className="text-xl leading-6">Deixe seu feedback</span>
-  
-  <CloseButton/>
-  </header>
-  
-  <div className="flex py-8 gap-2 w-full">
-  {Object.entries(feedbackTypes).map(([key,value])=> {
-    return (
-    <button
-    key={key}
-    className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center border-2 border-transparent hover:border-brand-500 focus:border-x-brand-500 focus:outline-none"
-    //inferencia manual de tipo
-    onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
-    
-    type="button"
-    >
-      <img src={value.image.source} alt={value.image.alt}></img>
-      <span>{value.title}</span>
-      </button>
-      )
-  })}
-</div>
-</>
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none transition-all duration-200"
+              onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
+    </>
   )
 }
