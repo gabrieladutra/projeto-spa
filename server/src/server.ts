951a648639310fb35fe238fd8e0e623667785f66
gabrieladutra <gabriela_ddutra@outlook.com>
2022-11-24 20:56:58 -0300

    Changed migration db to postegresql

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import cors from "cors";
-import { routes } from "./routes";
+import express from 'express';
+import cors from 'cors';
+import { routes } from './routes';
 
 const app = express();
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3333
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen({ port: port, hostname: "back-end" }, () => {
-    console.log(`HTTP server running on http://localhost:${port}`);
+app.listen({ port: port, hostname: 'back-end' }, () => {
+  console.log(`HTTP server running on http://localhost:${port}`)
 });
